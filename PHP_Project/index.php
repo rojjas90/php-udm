a97e9457f3ba58c7450678c5d3dd2cf6157dbf86
grojas <grojas@dwsoftware.mx>
2016-11-01 13:51:43 -0600

    25.- Creating controllers: Se crearon controladores basicos/necesarios para el proyecto, se referenciaron en el archivo principal (main.php)

--- PHP_Project/index.php
+++ PHP_Project/index.php
@@ -3,9 +3,17 @@
 
 // require('config.php');
 require('classes/Bootstrap.php');
+require('classes/Controller.php');
+
+// controllers
+require('controllers/home.php');
+require('controllers/users.php');
+require('controllers/shares.php');
+
 
 $bootstrap = new Bootstrap($_GET);
 $controller = $bootstrap->createController();
 
-
- ?>
+if ($controller) {
+  $controller->executeAction();
+}
